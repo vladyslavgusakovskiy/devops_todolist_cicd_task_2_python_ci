9eccf1e95362389a54c6f19f810ed453d528f2a4
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-05-25 16:06:40 +0300

    Added Python workflow

--- src/api/views.py
+++ src/api/views.py
@@ -10,6 +10,7 @@ import time
 
 startup_time = timezone.now()
 
+
 class IsCreatorOrReadOnly(permissions.BasePermission):
     """
     Object-level permission to only allow owners of an object to edit it.
@@ -60,9 +61,11 @@ class TodoViewSet(viewsets.ModelViewSet):
         creator = user if user.is_authenticated else None
         serializer.save(creator=creator)
 
+
 # Health Check View
 def health(request):
     return HttpResponse("Health OK", content_type="text/plain")
+
 
 # Readiness Check View
 def ready(request):
